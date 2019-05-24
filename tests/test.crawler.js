854f078c6ad6d3c9036d29facaa07ec9a337be4e
soronto3603 <soronto3603@gmail.com>
2019-05-24 14:51:49 +0900

    tempary

--- tests/test.crawler.js
+++ tests/test.crawler.js
@@ -1,7 +1,18 @@
 let crawler = require("../lib/crawler");
 console.log(crawler.NetflixCrawler(
     "https://www.netflix.com/kr/browse/genre/83",
-
+    // [
+    //     name : { 
+    //         T : /{.@.{7}L.{10}p.{9}[^,]*.{18}T.{10}n.{6}[^"]*.{3}u.{5}[^"]*/gi,
+    //         Pr : /{.@.{7}L.{10}p.{9}[^,]*.{18}T.{10}n.{6}/gi,
+    //         Po : /.{3}u.{5}[^"]*/gi
+    //     },
+    //     url : {
+    //         T : /{.@.{7}L.{10}p.{9}[^,]*.{18}T.{10}n.{6}[^"]*.{3}u.{5}[^"]*/gi,
+    //         Pr : /{.@.{7}L.{10}p.{9}[^,]*.{18}T.{10}n.{6}[^"]*.{3}u.{5}/gi,
+    //         Po : / /gi
+    //     }
+    // ]
     ));
 
 // console.log(crawler.get());
