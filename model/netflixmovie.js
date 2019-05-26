95d8fbc04caac3aca513034a628b9eb23bc73b66
soronto3603 <soronto3603@gmail.com>
2019-05-26 19:40:54 +0900

    change KEY name : actor => actors

--- model/netflixmovie.js
+++ model/netflixmovie.js
@@ -1,16 +1,16 @@
 class NetflixMovie{
-    constructor(name=null,url=null,year=null,actor=null){
+    constructor(name=null,url=null,year=null,actors=null){
         this.name = name;
         this.url = url;
         this.year = year;
-        if(typeof actor == "string"){
+        if(typeof actors == "string"){
             // actor string example.
             // "주지훈, 류승룡, 배두나"  
-            this.actor = actor.split(",");
-        }else if(typeof actor == "object"){
-            this.actor = actor;
+            this.actors = actors.split(",");
+        }else if(typeof actors == "object"){
+            this.actors = actors;
         }else{
-            this.actor = actor;
+            this.actors = actors;
         }
     }
 }
